759dbdc5778e5c21c0c78efaa9e801792a27850c
Danil Ardi <danilardi13@gmail.com>
2025-05-20 11:13:24 +0700

    refactor:notification code

--- src/app/notification/index.js
+++ src/app/notification/index.js
@@ -1,71 +1,88 @@
 import { isDevice } from "expo-device";
-import { addNotificationReceivedListener, addNotificationResponseReceivedListener, AndroidImportance, getExpoPushTokenAsync, getPermissionsAsync, removeNotificationSubscription, requestPermissionsAsync, scheduleNotificationAsync, setNotificationChannelAsync, setNotificationHandler } from "expo-notifications";
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
 import { useEffect, useRef, useState } from "react";
 import { Alert, Button, Platform, Text, View } from "react-native";
 
-setNotificationHandler({
+Notifications.setNotificationHandler({
     handleNotification: async () => ({
         shouldPlaySound: true,
-        shouldSetBadge: false,
+        shouldSetBadge: true,
         shouldShowBanner: true,
         shouldShowList: true,
     }),
 });
 
+function handleRegistrationError(errorMessage) {
+    alert(errorMessage);
+    throw new Error(errorMessage);
+}
+
 export default function NotificationsExample() {
     const [expoPushToken, setExpoPushToken] = useState('');
-    const notificationListener = useRef();
-    const responseListener = useRef();
 
     useEffect(() => {
-        registerForPushNotificationsAsync().then(token => {
-            if (token) {
-                setExpoPushToken(token);
-            }
+        registerForPushNotificationsAsync()
+            .then(token => setExpoPushToken(token ?? ''))
+            .catch((error) => setExpoPushToken(`${error}`));
+
+        const notificationListener = Notifications.addNotificationReceivedListener(notification => {
+            console.log('Notification received:', notification);
         });
 
-        notificationListener.current = addNotificationReceivedListener(notification => {
-            console.log('Notification received:', notification);
-        })
-
-        responseListener.current = addNotificationResponseReceivedListener(response => {
+        const responseListener = Notifications.addNotificationResponseReceivedListener(response => {
             console.log('Notification response received:', response);
             Alert.alert('Notification Response', `You tapped on the notification: ${response.notification.request.content.title}`);
-        })
+        });
 
         return () => {
-            removeNotificationSubscription(notificationListener.current);
-            removeNotificationSubscription(responseListener.current);
-        }
+            notificationListener.remove();
+            responseListener.remove();
+        };
+
     }, [])
 
     async function registerForPushNotificationsAsync() {
-        let token;
-        if (!isDevice) {
-            alert('Must use physical device for Push Notifications');
-            return;
-        }
-        const { status: existingStatus } = await getPermissionsAsync();
-        let finalStatus = existingStatus;
-        if (existingStatus !== 'granted') {
-            const { status } = await requestPermissionsAsync();
-            finalStatus = status;
-        }
-        if (finalStatus !== 'granted') {
-            alert('Failed to get push token for push notifications!');
-            return;
-        }
-        token = (await getExpoPushTokenAsync()).data
-        console.log('Expo Push Token:', token);
         if (Platform.OS === 'android') {
-            await setNotificationChannelAsync('default', {
+            Notifications.setNotificationChannelAsync('default', {
                 name: 'default',
-                importance: AndroidImportance.MAX,
+                importance: Notifications.AndroidImportance.MAX,
                 vibrationPattern: [0, 250, 250, 250],
                 lightColor: '#FF231F7C',
             });
         }
-        return token;
+
+        if (isDevice) {
+            const { status: existingStatus } = await Notifications.getPermissionsAsync();
+            let finalStatus = existingStatus;
+            if (existingStatus !== 'granted') {
+                const { status } = await Notifications.requestPermissionsAsync();
+                finalStatus = status;
+            }
+            if (finalStatus !== 'granted') {
+                handleRegistrationError('Permission not granted to get push token for push notification!');
+                return;
+            }
+            const projectId =
+                Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+            if (!projectId) {
+                handleRegistrationError('Project ID not found');
+            }
+            try {
+                const pushTokenString = (
+                    await Notifications.getExpoPushTokenAsync({
+                        projectId,
+                    })
+                ).data;
+                console.log(pushTokenString);
+                return pushTokenString;
+            } catch (e) {
+                handleRegistrationError(`${e}`);
+            }
+        } else {
+            handleRegistrationError('Must use physical device for push notifications');
+        }
     }
 
     const scheduleNotification = async () => {
